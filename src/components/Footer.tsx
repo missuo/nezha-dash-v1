cda717704ee2c1684b5e7548badc8f09661a5f4e
Vincent Yang <missuo@pm.me>
2026-02-28 02:59:37 +0800

    style(ui): remove italic and uppercase from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="mx-auto w-full max-w-5xl px-4 lg:px-0 pb-6 pt-2 server-footer">
       <section className="flex flex-col items-center gap-1 server-footer-name">
-        <p className="text-[13px] italic tracking-wide text-neutral-500/60 dark:text-neutral-400/50">Made with ♥️ with SF.</p>
-        <p className="text-[11px] tracking-widest uppercase text-neutral-400/50 dark:text-neutral-500/40">Powered by OwO Network, LLC</p>
+        <p className="text-[13px] text-neutral-500/60 dark:text-neutral-400/50">Made with ♥️ in SF.</p>
+        <p className="text-[11px] text-neutral-400/50 dark:text-neutral-500/40">Powered by OwO Network, LLC</p>
       </section>
     </footer>
   )
